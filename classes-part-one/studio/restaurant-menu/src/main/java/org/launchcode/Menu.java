9cf6450a039478768ce7e1c1024b73825877ebcf
Colin Robinson <colin@Colins-Air.lan>
2023-08-28 19:35:48 -0500

    Finished part one of the Class 3 studio.

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -4,27 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
-    private Date lastUpdated;
-    private ArrayList<MenuItem> items;
+    private Date menuDate;
+    private ArrayList<MenuItem> menuItem = new ArrayList<>();
 
-    public Menu(Date d, ArrayList<MenuItem> i) {
-        this.lastUpdated = d;
-        this.items = i;
-    }
 
-    public void setLastUpdated(Date lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public void setItems(ArrayList<MenuItem> items) {
-        this.items = items;
-    }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public ArrayList<MenuItem> getItems() {
-        return items;
-    }
 }
